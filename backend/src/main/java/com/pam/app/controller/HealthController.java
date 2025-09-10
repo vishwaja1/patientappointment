4be39252943c63ca22ff22f68121e67068a04e8c
vishwaja1 <42867589+vishwaja1@users.noreply.github.com>
2025-09-09 17:34:14 -0700

    Create HealthController.java

---
+++ backend/src/main/java/com/pam/app/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.pam.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/actuator/health")
+    public String health() {
+        return "UP";
+    }
+}
